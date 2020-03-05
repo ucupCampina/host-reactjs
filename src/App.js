14504ad5f8f37d708bcff42a3871892d0c2adaab
IhsanKirson <mnurihsanmutaqin66.siswa@smkn4bdg.sch.id>
2020-03-05 21:23:06 +0700

    Ihsan coba host reactjs

--- src/App.js
+++ src/App.js
@@ -1,26 +1,114 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from "react";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 import './App.css';
 
-function App() {
+function Home() {
+  return <h2> Halaman Home </h2>
+}
+
+function ListView() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2> Semua User </h2>
+      <ul>
+        <Link to='user/ihsan'> Ihsan </Link>
+        <br />
+        <Link to='user/robert'> Robert </Link>
+      </ul>
     </div>
   );
+}
+
+function DetailView({ match }) {
+  return <h2> Ini Halaman {match.params.name} </h2>
+}
+
+function NoMatch() {
+  return <h2> 404, halaman tidak ditemukan </h2>
+}
+
+class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      isLoading: true
+    }
+  }
+
+  // Komsumsi API
+  componentDidMount() {
+    this.setState({
+      isLoading: false
+    })
+  }
+
+  render() {
+
+    const { isLoading } = this.state
+
+    if (isLoading) {
+      return <p>Loading.....</p>
+    }
+
+    return (
+      <BrowserRouter>
+        <div>
+          <nav>
+            <li> <Link to='/react-host/'> Home </Link> </li>
+            <li> <Link to='/react-host/users'> User </Link> </li>
+            <li> <Link to='/react-host/tabel'> Tabel </Link> </li>
+          </nav>
+
+          <main>
+            <Switch>
+              <Route path='/react-host/' exact component={Home} />
+              <Route path='/react-host/users' exact component={ListView} />
+              <Route path='/react-host/user/:name' exact component={DetailView} />
+              <Route path='/react-host/tabel' exact >
+                    <Column />
+              </Route>
+              <Route component={NoMatch} />
+            </Switch>
+          </main>
+        </div>
+      </BrowserRouter>
+    );
+  }
+
+}
+
+class Column extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      items: []
+    }
+  }
+
+  // Komsumsi API
+  componentDidMount() {
+    fetch("http://jsonplaceholder.typicode.com/users")
+      .then(Response => Response.json())
+      .then(data => this.setState({ items: data }))
+  }
+
+  render() {
+
+    const { items } = this.state
+
+    return (
+      <div>
+        <h2>Data User</h2>
+        <ul>
+          {items.map((item, index) =>
+            <li key={index}> {item.name} </li>)}
+        </ul>
+      </div>
+    );
+  }
+
 }
 
 export default App;
